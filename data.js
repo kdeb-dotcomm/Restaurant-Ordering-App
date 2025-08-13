f735a5e591c68b4add33e825e0bfffab0d99001e
kdeb-dotcomm <eugenedebrah07@gmail.com>
2025-08-13 16:48:00 +0000

    on the project

---
+++ data.js
@@ -0,0 +1,43 @@
+export const item = [
+    {
+        name : 'Pizza',
+        emoji: '🍕',
+        decription: 'pepperoni,mushroom,mozzarella',
+        price: 14,
+    },
+
+    {
+        name : 'Hamburger',
+        emoji: '🍔',
+        decription: 'beef,cheese,lettuce,tomato',
+        price: 12,
+    },
+
+    {
+        name : 'Pasta',
+        emoji: '🍝',
+        decription: 'spaghetti,meatballs,parmesan',
+        price: 12,
+    },
+
+    {
+        name : 'Salad',
+        emoji: '🥗',
+        decription: 'mixed greens,tomato,cucumber',
+        price: 8,
+    },
+
+    {
+        name : 'Sushi',
+        emoji: '🍣',
+        decription: 'salmon,avocado,rice',
+        price: 16,
+    },
+
+    {
+        name : 'Beer',
+        emoji: '🍺',
+        description : 'grain,hops,yeast,water',
+        price: 12,
+    }
+]
---
+++ script.js
@@ -0,0 +1,3 @@
+import { item } from "./data.js";
+
+console.log(item);
